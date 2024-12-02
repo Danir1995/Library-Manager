31daad17810b51eae68680f446d70db6521000d7
DanirPT <danir.hucaev@gmail.com>
2024-12-02 22:03:25 +0000

    added main class LibraryManagerApplication

---
+++ .idea/src/main/java/com/danir/libraryManager/LibraryManagerApplication.java
@@ -0,0 +1,16 @@
+package com.danir.libraryManager;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@SpringBootApplication
+public class LibraryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerApplication.class, args);
+    }
+
+}
