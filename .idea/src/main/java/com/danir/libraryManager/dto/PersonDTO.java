0c4a3a6a6be38d74bdd2dc478b57f803f73d9ee2
DanirPT <danir.hucaev@gmail.com>
2024-12-04 20:01:58 +0000

    added PersonDTO

---
+++ .idea/src/main/java/com/danir/libraryManager/dto/PersonDTO.java
@@ -0,0 +1,18 @@
+package com.danir.libraryManager.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+
+    @Size(min = 2, max = 32,message = "name must be between 2 and 32 characters")
+    @NotNull(message = "full name can not be empty")
+    private String fullName;
+
+    @Min(value = 1920, message = "Year can not be less than 1920")
+    @NotNull(message = "fill this line")
+    private int yearOfBirth;
+}
